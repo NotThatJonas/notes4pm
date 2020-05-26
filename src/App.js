6601837da947bfe6b098eff043ba5f07033077f8
NotThatJonas <mr.joshuajonas@gmail.com>
2020-05-25 19:56:40 -0700

    made some more changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import axios from 'axios';
 
-class App extends React.Component {
+class App extends React.Component{
+  
   constructor(props) {
 	super(props);
 	this.state = {
@@ -8,6 +10,27 @@ class App extends React.Component {
   	email: '',
   	message: ''
 	}
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    axios({
+      method: "POST", 
+      url:"http://localhost:3002/send", 
+      data:  this.state
+    }).then((response)=>{
+      if (response.data.status === 'success'){
+        alert("Message Sent."); 
+        this.resetForm()
+      }else if(response.data.status === 'fail'){
+        alert("Message failed to send.")
+      }
+    })
+  }
+
+  resetForm(){
+    
+     this.setState({name: "", email: "", message: ""})
   }
 
 render() {
